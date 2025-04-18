a30d031c60d4538a5c7dc7a6bffe74e259ab7c18
jcw1031 <jcw001031@gmail.com>
2025-04-18 20:05:46 +0900

    config: 통합테스트 환경 구성

--- src/integrationTest/java/com/woopaca/noongil/NoongilApplicationTest.java
+++ src/integrationTest/java/com/woopaca/noongil/NoongilApplicationTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class NoongilApplicationTests {
+class NoongilApplicationTest {
 
     @Test
     void contextLoads() {
---
+++ src/test/java/com/woopaca/noongil/NoongilApplicationTest.java
@@ -0,0 +1,13 @@
+package com.woopaca.noongil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoongilApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
