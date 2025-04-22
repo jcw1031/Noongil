70a5c6bbea8c248209a709f7d8d7a34929bf5fea
jcw1031 <jcw001031@gmail.com>
2025-04-22 23:31:43 +0900

    test: xml 파싱 테스트

--- src/main/java/com/woopaca/noongil/infrastructure/notification/ApnsPushNotificationSender.java
+++ src/main/java/com/woopaca/noongil/infrastructure/notification/ApnsPushNotificationSender.java
@@ -1,6 +1,8 @@
 package com.woopaca.noongil.infrastructure.notification;
 
 import com.eatthepath.pushy.apns.ApnsClient;
+import com.eatthepath.pushy.apns.util.SimpleApnsPushNotification;
+import com.woopaca.noongil.infrastructure.apple.AppleProperties;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
@@ -9,8 +11,14 @@ import org.springframework.stereotype.Component;
 public class ApnsPushNotificationSender implements PushNotificationSender {
 
     private final ApnsClient apnsClient;
+    private final AppleProperties appleProperties;
 
-    public ApnsPushNotificationSender(ApnsClient apnsClient) {
+    public ApnsPushNotificationSender(ApnsClient apnsClient, AppleProperties appleProperties) {
         this.apnsClient = apnsClient;
+        this.appleProperties = appleProperties;
+    }
+
+    public void test() {
+        new SimpleApnsPushNotification("pushToken", appleProperties.getClientId(), null);
     }
 }
